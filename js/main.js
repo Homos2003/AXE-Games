07515824b16356f03b2ba179d162ca3631178250
Homos2003 <homoshegazy@gmail.com>
2024-01-09 16:18:07 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+//  Navbar
+
+let btn = document.querySelector(".toggle-menu");
+let links = document.getElementById("links");
+
+btn.addEventListener("click", function () {
+  links.classList.toggle("active");
+});
+
+
+// Information About Game
+
+let det = document.getElementsByClassName("deta");
+let ico = document.getElementsByClassName("icon");
+
+for (let i = 0; i < det.length; i++) {
+  det[i].addEventListener("click", function () {
+    ico[i].classList.toggle("active");
+    let parent = this.parentElement;
+    if (parent.style.maxHeight) {
+      parent.style.maxHeight = null;
+      parent.style.backgroundColor = "transparent";
+    } else {
+      parent.style.maxHeight = parent.scrollHeight = 200 + "px";
+      parent.style.backgroundColor = "#2c323d";
+    }
+  });
+}
+
+// New tabs Game
+let tabs = document.querySelectorAll(".tabs li");
+let tabsArray = Array.from(tabs);
+let divs = document.querySelectorAll(".container > .game-card");
+let divsArray = Array.from(divs);
+
+tabsArray.forEach((ele) => {
+  ele.addEventListener("click", function (e) {
+    tabsArray.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+    divsArray.forEach((div) => {
+      div.style.display = "none";
+    });
+    document.querySelector(e.currentTarget.dataset.cont).style.display = "grid";
+  });
+});
